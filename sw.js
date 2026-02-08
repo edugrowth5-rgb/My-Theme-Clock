7639a753df5076c331a5eb6c885e88dfed716f15
edugrowth5-rgb <edugrowth5@gmail.com>
2026-02-08 20:21:34 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'sagar-clock-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
